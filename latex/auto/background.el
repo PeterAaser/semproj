996981ad68602fe6aca3cf03b7666393490d7946
Peter Aaser <peteraaser@gmail.com>
2016-12-06 17:37:09 +0100

    Split files. Play with tikz. Add words and pix

---
+++ latex/auto/background.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "background"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:generic_MEA"
+    "fig:st_olav_MEA"
+    "fig:headstage"
+    "fig:neuron_anatomy"
+    "fig:IFB_regular"
+    "fig:IFB_DSP"))
+ :latex)
+
---
+++ latex/auto/tikz.el
@@ -0,0 +1,8 @@
+(TeX-add-style-hook
+ "tikz"
+ (lambda ()
+   (TeX-add-symbols
+    "blockdist"
+    "edgedist"))
+ :latex)
+
