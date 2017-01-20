6dbd50a65435f1855310046707fb0950d6be3c36
Peter Aaser <peteraaser@gmail.com>
2017-01-20 11:54:36 +0100

    hehe

---
+++ auto/tikz.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "tikz"
+ (lambda ()
+   (TeX-add-symbols
+    "layersep"
+    "preInputNeurons"
+    "preHiddenNeurons"
+    "preOutputNeurons"
+    "OutInputNeurons"
+    "OutHiddenNeurons"
+    "OutOutputNeurons"))
+ :latex)
+
